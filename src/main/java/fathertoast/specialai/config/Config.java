3f47ee70b6c875b109ce7544f2a2d946486dd332
FatherToast <father.toast@hotmail.com>
2021-07-17 18:36:01 -0500

    Setup workspace for MC 1.16.5 with copy/pasted bootstraps from SAI 1.0.1.

---
+++ src/main/java/fathertoast/specialai/config/Config.java
@@ -0,0 +1,59 @@
+package fathertoast.specialai.config;
+
+import fathertoast.specialai.ModCore;
+import fathertoast.specialai.config.field.AbstractConfigField;
+import fathertoast.specialai.config.file.ToastConfigSpec;
+import fathertoast.specialai.config.file.TomlHelper;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.io.File;
+
+/**
+ * Used as the sole hub for all config access from outside the config package.
+ * <p>
+ * Contains references to all config files used in this mod, which in turn provide direct 'getter' access to each
+ * configurable value.
+ */
+public class Config {
+    /** The root folder for config files in this mod. */
+    public static final File CONFIG_DIR = new File( FMLPaths.CONFIGDIR.get().toFile(), "FatherToast/" + ModCore.MOD_ID + "/" );
+    
+    public static final GeneralConfig GENERAL = new GeneralConfig( CONFIG_DIR, "general" );
+    
+    /** Performs initial loading of all configs in this mod. */
+    public static void initialize() {
+        AbstractConfigField.loadingCategory = null;
+        
+        GENERAL.SPEC.initialize();
+    }
+    
+    /**
+     * Represents one config file that contains a reference for each configurable value within and a specification
+     * that defines the file's format.
+     */
+    public static abstract class AbstractConfig {
+        /** The spec used by this config that defines the file's format. */
+        public final ToastConfigSpec SPEC;
+        
+        AbstractConfig( File dir, String fileName, String... fileDescription ) {
+            AbstractConfigField.loadingCategory = "";
+            SPEC = new ToastConfigSpec( dir, fileName );
+            SPEC.header( TomlHelper.newComment( fileDescription ) );
+        }
+    }
+    
+    /**
+     * Represents one config file that contains a reference for each configurable value within and a specification
+     * that defines the file's format.
+     */
+    public static abstract class AbstractCategory {
+        /** The spec used by this config that defines the file's format. */
+        protected final ToastConfigSpec SPEC;
+        
+        AbstractCategory( ToastConfigSpec parent, String name, String... categoryDescription ) {
+            AbstractConfigField.loadingCategory = name + ".";
+            SPEC = parent;
+            SPEC.category( name, TomlHelper.newComment( categoryDescription ) );
+        }
+    }
+}
---
+++ src/main/java/fathertoast/specialai/config/GeneralConfig.java
@@ -0,0 +1,93 @@
+package fathertoast.specialai.config;
+
+import fathertoast.specialai.config.field.*;
+import fathertoast.specialai.config.file.ToastConfigSpec;
+import fathertoast.specialai.config.util.BlockList;
+import fathertoast.specialai.config.util.EntityEntry;
+import fathertoast.specialai.config.util.EntityList;
+import net.minecraft.entity.EntityType;
+
+import java.io.File;
+
+public class GeneralConfig extends Config.AbstractConfig {
+    
+    public final General GENERAL;
+    
+    /** Builds the config spec that should be used for this config. */
+    GeneralConfig( File dir, String fileName ) {
+        super( dir, fileName,
+                "This config contains options for several miscellaneous features in the mod, such as:",
+                "NOTHING, YET!"
+        );
+    
+        SPEC.newLine();
+        SPEC.describeEntityList();
+        SPEC.newLine();
+        SPEC.describeBlockList();
+        
+        GENERAL = new General( SPEC );
+    }
+    
+    public static class General extends Config.AbstractCategory {
+        
+        public final EntityListField.Combined entityList;
+        
+        public final BooleanField requiresTarget;
+        public final BooleanField requiresTools;
+        
+        public final BooleanField leaveDrops;
+        
+        public final DoubleField breakSpeed;
+        public final BooleanField madCreepers;
+        
+        public final BooleanField targetDoors;
+        public final BlockListField.Combined targetList;
+        
+        General( ToastConfigSpec parent ) {
+            super( parent, "general",
+                    "Options to customize NOTHING AT ALL." );
+            
+            entityList = new EntityListField.Combined(
+                    SPEC.define( new EntityListField( "entities.whitelist", new EntityList(
+                            new EntityEntry( EntityType.ZOMBIE, 1.0 ), new EntityEntry( EntityType.CREEPER, 1.0 )
+                    ).setSinglePercent(),
+                            "List of mobs that can gain door breaking AI (note that the entity must have task-based AI enabled).",
+                            "Additional value after the entity type is the chance (0.0 to 1.0) for entities of that type to spawn with the AI." ) ),
+                    SPEC.define( new EntityListField( "entities.blacklist", new EntityList().setNoValues() ) )
+            );
+            
+            SPEC.newLine();
+            
+            requiresTarget = SPEC.define( new BooleanField( "require_target", true,
+                    "If true, mobs will only break doors while they are chasing an attack target.",
+                    "Disabling this typically leads to mobs smashing into your house to get to blocks they are targeting",
+                    "as part of an idle griefing or fiddling behavior, such as torches or chests." ) );
+            requiresTools = SPEC.define( new BooleanField( "require_tools", true,
+                    "If true, mobs will only break doors they have the tools to harvest.",
+                    "For example, they will only break iron doors if they have a pickaxe." ) );
+            
+            SPEC.newLine();
+            
+            leaveDrops = SPEC.define( new BooleanField( "leave_drops", true,
+                    "If true, doors broken by mobs will leave item drops." ) );
+            
+            SPEC.newLine();
+            
+            breakSpeed = SPEC.define( new DoubleField( "break_speed", 0.33, DoubleField.Range.POSITIVE,
+                    "The block breaking speed multiplier for mobs breaking doors, relative to the player's block breaking speed." ) );
+            madCreepers = SPEC.define( new BooleanField( "mad_creepers", true,
+                    "If true, creepers will resort to what they know best when they meet a door blocking their path." ) );
+            
+            SPEC.newLine();
+            
+            targetDoors = SPEC.define( new BooleanField( "targets.auto_target_doors", true,
+                    "If true, door breaking AI will automatically target all blocks that derive from the",
+                    "vanilla doors, fence gates, and trapdoors." ) );
+            targetList = new BlockListField.Combined(
+                    SPEC.define( new BlockListField( "targets.whitelist", new BlockList(),
+                            "List of blocks that that can be broken by the door breaking AI." ) ),
+                    SPEC.define( new BlockListField( "targets.blacklist", new BlockList() ) )
+            );
+        }
+    }
+}
