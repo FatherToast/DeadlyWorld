862ac4b93d79a664f83a467fac23e62d85c12809
FatherToast <father.toast@hotmail.com>
2021-07-18 11:47:52 -0500

    Updated en_us lang file, carried over todo list

--- src/main/java/fathertoast/deadlyworld/ModCore.java
+++ src/main/java/fathertoast/deadlyworld/ModCore.java
@@ -14,44 +14,39 @@ import javax.annotation.Nullable;
 @Mod( ModCore.MOD_ID )
 public class ModCore {
     /* TODO LIST:
-     *  - Re-implement features:
-     *      + Villages
+     *  - everything
+     *  - upgrade config to allow per-dimension files
+     *  - veins
+     *  - features
+     *  - silverfish block
      *
      * Primary features:
-     *  - New Elite AIs:
-     *      + slam: wields an axe, performs knockback slam "explosion" when damaged in close range
-     *      + fishing: wields a fishing rod and uses it against its target
-     *      + grappling: similar to fishing, but pulls the mob to target instead, mob may mount the target?
-     *  - Reactions:
-     *      + better response to being sniped
-     *  - Door Breaking:
-     *      + fix pathfinding to path through valid doors - is this possible without a mixin on WalkNodeProcessor?
-     *  - Use Item Attack: Replace mob attack AI with "item use", which simulates item use for its "attack"
-     *  - Dig: break down walls when unable to path to target
-     *  - Stalk: try to hide behind blocks when target is looking in their general direction
-     *  - Portals: walk into nearby portals (part of idle fiddling?)
-     *  - Counter-potions: reactive potion drinking (similar to witches) from a small "inventory"
-     *      + health (damage for undead) when low on health
-     *	    + fire resistance when on fire or taking fire damage
-     *	    + water breathing when drowning
-     *	    ? strength when target is armored/enchanted
-     *	    ? invisibility when shot by arrows
-     *  - Bosses: Rare, powerful monsters
-     *      + Nemesis system? (See Darkmega)
-     *      + Name influenced by entity type
-     *      + Uses a "unique" item that is a guaranteed drop
-     *      + Unique item is enchanted and has a special enchantment/modifier
-     *      + The special modifier is more powerful than otherwise obtainable, but may come with a drawback
-     *      + Unique item is named based on its special (prefixed by "<boss_name>'s")
-     *      + Uses the counter-potion system
-     *	? Flock: hunt together
-     *  ? Spread fire: ignite flammable blocks when on fire and chasing a target
+     *  - chests
+     *      + mimic 2.0 (custom entity)
+     *      + cave-in (via surprise or combo)
+     *  - water traps
+     *      + vortex
+     *  - floor traps
+     *      + fire
+     *  - ceiling traps
+     *      + cave-in
+     *      + lava
+     *  - combo traps
+     *      + spider spawner & splash poison dispenser
+     *      + undead spawner & splash harm dispener
+     *      ? any spawner & fish hook dispenser (custom entity)
+     *      ? any floor trap & fish hook dispenser (custom entity)
+     *      ? fire immune spawner & fireball dispenser
+     *      ? creeper spawner & lightning dispenser
+     *  ? support for custom potions in towers/floor traps/events
+     *  ? wall traps
+     *      + arrow traps
+     *  - config tweaks
+     *      ? option to allow floor traps to trigger vs creative mode players, and vice-versa for other traps
      *
      * Utility features:
-     *  - Fix squid not pathing at all and fish moving super slowly in xz-plane
-     *  - More editing for AI, particularly for editing pre-existing AI
-     *  - An extra whitelist for each elite AI to apply it directly
-     *  - More effective configs, more per-entity-id options
+     *  - modify vanilla structures?
+     *  ? add chance to fail replacing blocks in config (notably per silverfish replaceable block and per vein)
      */
     
     /** The mod id and namespace used by this mod. */
