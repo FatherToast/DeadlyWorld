38ccf23962816f5aa51438b53e86b8878206b7b2
FatherToast <FatherToast@users.noreply.github.com>
2022-01-14 19:56:46 -0600

    obs

--- src/main/java/fathertoast/deadlyworld/block/state/IExclusiveMetaProvider.java
+++
@@ -1,9 +0,0 @@
-package fathertoast.deadlyworld.block.state;
-
-import net.minecraft.util.IStringSerializable;
-
-public
-interface IExclusiveMetaProvider extends IStringSerializable
-{
-	int getMetadata( );
-}
