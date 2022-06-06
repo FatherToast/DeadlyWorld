bc6766e9760000ef719e592c8f5611ff8b695972
FatherToast <father.toast@hotmail.com>
2022-06-05 20:30:50 -0500

    maybe more mini mob murdering

--- src/main/java/fathertoast/deadlyworld/common/entity/MiniCreeperEntity.java
+++ src/main/java/fathertoast/deadlyworld/common/entity/MiniCreeperEntity.java
@@ -1,46 +1,56 @@
 package fathertoast.deadlyworld.common.entity;
 
 import fathertoast.deadlyworld.common.entity.ai.MiniCreeperSwellGoal;
+import fathertoast.deadlyworld.common.registry.DWEntities;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.monster.CreeperEntity;
-import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class MiniCreeperEntity extends CreeperEntity {
-
-    public MiniCreeperEntity(EntityType<? extends CreeperEntity> entityType, World world) {
-        super(entityType, world);
-        this.explosionRadius = 1;
-    }
-
+    
+    public MiniCreeperEntity( EntityType<? extends CreeperEntity> entityType, World world ) { super( entityType, world ); }
+    
     public static AttributeModifierMap.MutableAttribute createAttributes() {
-        return MonsterEntity.createMonsterAttributes()
-                .add(Attributes.MOVEMENT_SPEED, 0.35D)
-                .add(Attributes.MAX_HEALTH, 4.0D);
+        return DWEntities.standardMiniAttributes( CreeperEntity.createAttributes(), 0.25 );
     }
-
+    
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new SwimGoal(this));
-        this.goalSelector.addGoal(2, new MiniCreeperSwellGoal(this));
-        this.goalSelector.addGoal(3, new AvoidEntityGoal<>(this, OcelotEntity.class, 6.0F, 1.0D, 1.2D));
-        this.goalSelector.addGoal(3, new AvoidEntityGoal<>(this, CatEntity.class, 6.0F, 1.0D, 1.2D));
-        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, false));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 0.8D));
-        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 8.0F));
-        this.goalSelector.addGoal(6, new LookRandomlyGoal(this));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
-        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+        this.goalSelector.addGoal( 1, new SwimGoal( this ) );
+        this.goalSelector.addGoal( 2, new MiniCreeperSwellGoal( this ) );
+        this.goalSelector.addGoal( 3, new AvoidEntityGoal<>( this, OcelotEntity.class, 6.0F, 1.0, 1.2 ) );
+        this.goalSelector.addGoal( 3, new AvoidEntityGoal<>( this, CatEntity.class, 6.0F, 1.0, 1.2 ) );
+        this.goalSelector.addGoal( 4, new MeleeAttackGoal( this, 1.0, false ) );
+        this.goalSelector.addGoal( 5, new WaterAvoidingRandomWalkingGoal( this, 0.8 ) );
+        this.goalSelector.addGoal( 6, new LookAtGoal( this, PlayerEntity.class, 8.0F ) );
+        this.goalSelector.addGoal( 6, new LookRandomlyGoal( this ) );
+        this.targetSelector.addGoal( 1, new NearestAttackableTargetGoal<>( this, PlayerEntity.class, true ) );
+        this.targetSelector.addGoal( 2, new HurtByTargetGoal( this ) );
     }
-
+    
     @Override
-    public int getMaxAirSupply() {
-        return 100;
+    public int getMaxAirSupply() { return 100; }
+    
+    @Override
+    public void explodeCreeper() {
+        if( !this.level.isClientSide ) {
+            Explosion.Mode explosion$mode = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent( this.level, this ) ? Explosion.Mode.DESTROY : Explosion.Mode.NONE;
+            float f = (this.isPowered() ? 2.0F : 1.0F) / 2.0F; // Only change from vanilla method, halve explosion power
+            this.dead = true;
+            this.level.explode( this, this.getX(), this.getY(), this.getZ(), (float) this.explosionRadius * f, explosion$mode );
+            this.remove();
+            this.spawnLingeringCloud();
+        }
     }
 }
--- src/main/java/fathertoast/deadlyworld/common/entity/MiniZombieEntity.java
+++ src/main/java/fathertoast/deadlyworld/common/entity/MiniZombieEntity.java
@@ -1,49 +1,44 @@
 package fathertoast.deadlyworld.common.entity;
 
+import fathertoast.deadlyworld.common.registry.DWEntities;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.EntitySize;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Pose;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.world.World;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class MiniZombieEntity extends ZombieEntity {
-
-    public MiniZombieEntity(EntityType<? extends ZombieEntity> entityType, World world) {
-        super(entityType, world);
+    
+    public MiniZombieEntity( EntityType<? extends ZombieEntity> entityType, World world ) {
+        super( entityType, world );
     }
-
+    
     public static AttributeModifierMap.MutableAttribute createAttributes() {
-        return MonsterEntity.createMonsterAttributes()
-                .add(Attributes.MAX_HEALTH, 5.0D)
-                .add(Attributes.FOLLOW_RANGE, 25.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.30F)
-                .add(Attributes.ATTACK_DAMAGE, 1.0D)
-                .add(Attributes.ARMOR, 0.0D)
-                .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
+        return DWEntities.standardMiniAttributes( ZombieEntity.createAttributes(), 0.23 )
+                .add( Attributes.FOLLOW_RANGE, 25.0 )
+                .add( Attributes.ATTACK_DAMAGE, 3.0 / 2.0 );
     }
-
+    
     /** Do not allow for mini baby zombies to exist. */
     @Override
-    public void setBaby(boolean isBaby) {
-        super.setBaby(false);
-    }
-
+    public void setBaby( boolean isBaby ) { super.setBaby( false ); }
+    
     @Override
-    public boolean canBreakDoors() {
-        return false;
-    }
-
-    // TODO - Consider mini drowned?
+    public boolean canBreakDoors() { return false; }
+    
+    // Should we consider mini drowned?
     @Override
-    protected boolean convertsInWater() {
-        return false;
-    }
-
+    protected boolean convertsInWater() { return false; }
+    
     @Override
-    protected float getStandingEyeHeight(Pose pose, EntitySize entitySize) {
+    protected float getStandingEyeHeight( Pose pose, EntitySize entitySize ) {
         return 0.7F;
     }
 }
