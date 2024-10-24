4b14df19f192842722d233ec7a8cabc6053a374c
Sarinsa <mikkemusbruker@gmail.com>
2024-10-24 19:15:13 +0200

    Data gen moment

--- src/main/java/fathertoast/deadlyworld/datagen/DataGatherer.java
+++ src/main/java/fathertoast/deadlyworld/datagen/DataGatherer.java
@@ -3,26 +3,40 @@ package fathertoast.deadlyworld.datagen;
 import fathertoast.deadlyworld.common.core.DeadlyWorld;
 import fathertoast.deadlyworld.datagen.loot.DWLootTableProvider;
 import fathertoast.deadlyworld.datagen.tags.DWBlockTagsProvider;
+import fathertoast.deadlyworld.datagen.worldgen.DWFeatureProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber( modid = DeadlyWorld.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD )
 public class DataGatherer {
-    
+
+
+    /** Data provider that generates registry entries from supported registry types. */
+    private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, DWFeatureProvider::bootstrapConfigured)
+            .add(Registries.PLACED_FEATURE, DWFeatureProvider::bootstrapPlaced);
+
+
+
     @SubscribeEvent
     public static void onGatherData( GatherDataEvent event ) {
         final DataGenerator generator = event.getGenerator();
         final PackOutput packOutput = generator.getPackOutput();
         final CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         final ExistingFileHelper fileHelper = event.getExistingFileHelper();
-        
+
+
         if( event.includeClient() ) {
             //            generator.addProvider( true, new SMBlockStateAndModelProvider( packOutput, fileHelper ) );
             //            generator.addProvider( true, new SMItemModelProvider( packOutput, fileHelper ) );
@@ -31,6 +45,7 @@ public class DataGatherer {
             //            }
         }
         if( event.includeServer() ) {
+            generator.addProvider( true, new DatapackBuiltinEntriesProvider( packOutput, lookupProvider, BUILDER, Set.of( DeadlyWorld.MOD_ID ) ) );
             generator.addProvider( true, new DWLootTableProvider( packOutput ) );
             generator.addProvider( true, new DWBlockTagsProvider( packOutput, lookupProvider, fileHelper ) );
         }
--- src/main/java/fathertoast/deadlyworld/datagen/worldgen/DWFeatureProvider.java
+++ src/main/java/fathertoast/deadlyworld/datagen/worldgen/DWFeatureProvider.java
@@ -1,4 +1,65 @@
 package fathertoast.deadlyworld.datagen.worldgen;
 
+import fathertoast.deadlyworld.common.core.DeadlyWorld;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.LakeFeature;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.placement.*;
+
+import java.util.List;
+
 public class DWFeatureProvider {
+
+
+    public static final ResourceKey<ConfiguredFeature<?, ?>> SPONGE_LAKE = configuredKey("sponge_lake");
+
+    public static final ResourceKey<PlacedFeature> PLACED_SPONGE_LAKE_SURFACE = placedKey("sponge_lake_surface");
+
+
+
+    /** Called by registry set builder to generate our configured features. */
+    public static void bootstrapConfigured(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        register(context, DWFeatureProvider.SPONGE_LAKE, new ConfiguredFeature<>(Feature.LAKE, new LakeFeature.Configuration(BlockStateProvider.simple(Blocks.SPONGE.defaultBlockState()), BlockStateProvider.simple(Blocks.DIRT.defaultBlockState()))));
+    }
+
+
+    /** Called by registry set builder to generate our placed features. */
+    public static void bootstrapPlaced(BootstapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> getter = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        final Holder<ConfiguredFeature<?, ?>> SPONGE_LAKE = getter.getOrThrow(DWFeatureProvider.SPONGE_LAKE);
+
+        register(context, DWFeatureProvider.PLACED_SPONGE_LAKE_SURFACE, SPONGE_LAKE, List.of(RarityFilter.onAverageOnceEvery(100), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE, BiomeFilter.biome()));
+    }
+
+
+    // Just some convenience methods below
+
+    protected static void register(BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> confFeatureKey, ConfiguredFeature<?, ?> configuredFeature) {
+        context.register(confFeatureKey, configuredFeature);
+    }
+
+    protected static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> placedFeatureKey, Holder<ConfiguredFeature<?, ?>> configuredFeature, PlacementModifier... modifiers) {
+        register(context, placedFeatureKey, configuredFeature, List.of(modifiers));
+    }
+
+    protected static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> placedFeatureKey, Holder<ConfiguredFeature<?, ?>> configuredFeature, List<PlacementModifier> modifiers) {
+        context.register(placedFeatureKey, new PlacedFeature(configuredFeature, modifiers));
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> configuredKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, DeadlyWorld.resourceLoc(name));
+    }
+
+    public static ResourceKey<PlacedFeature> placedKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, DeadlyWorld.resourceLoc(name));
+    }
 }
