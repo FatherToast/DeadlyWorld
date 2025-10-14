00370d3b45511b4a804b6b67ead931a865506753
Sarinsa <mikkemusbruker@gmail.com>
2025-10-14 03:40:56 +0200

    Explotando

--- src/main/java/fathertoast/deadlyworld/common/block/sea_mine/SeaMineBlock.java
+++ src/main/java/fathertoast/deadlyworld/common/block/sea_mine/SeaMineBlock.java
@@ -11,12 +11,16 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChainBlock;
@@ -29,12 +33,14 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.ticks.TickPriority;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.function.Predicate;
 
 @SuppressWarnings( "deprecation" )
 public class SeaMineBlock extends Block implements SimpleWaterloggedBlock {
@@ -45,6 +51,9 @@ public class SeaMineBlock extends Block implements SimpleWaterloggedBlock {
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final BooleanProperty ARMED = BooleanProperty.create( "armed" );
+
+    private static final Predicate<Player> DEFAULT_PREDICATE = (player) -> !player.isSpectator() && !player.isCreative();
+    private static final Predicate<Player> VS_CREATIVE_PREDICATE = (player) -> !player.isSpectator();
 
     private final SeaMineType type;
 
@@ -80,32 +89,21 @@ public class SeaMineBlock extends Block implements SimpleWaterloggedBlock {
         // We are armed, explotando time
         if ( armed ) {
             level.removeBlock( pos, false );
-            level.explode(
-                    null,
-                    pos.getX() + 0.5D,
-                    pos.getY() + 0.5D,
-                    pos.getZ() + 0.5D,
-                    getExplosionPower(),
-                    Level.ExplosionInteraction.BLOCK
-            );
-            // Pick a potion and apply it to all creatures caught in the blast
-            if ( getPotionList().isEmpty() ) return;
-
-            final MobEffectInstance potion = getPotionList().next( random );
-
-            if ( potion == null ) return;
-
-            List<LivingEntity> nearbyCreatures = level.getEntitiesOfClass( LivingEntity.class, new AABB( pos ).inflate( getExplosionPower() ) );
-            nearbyCreatures.forEach( (livingEntity) ->
-                    livingEntity.addEffect( new MobEffectInstance( potion ) ) );
         }
         // Check for a nearby player in survival or adventure mode
         else {
+            // Do not arm the mine in peaceful mode unless configured to
+            if ( level.getDifficulty() == Difficulty.PEACEFUL && !Config.MAIN.GENERAL.activateTrapsInPeaceful.get() )
+                return;
+
             List<Player> nearbyPlayers = level.getEntitiesOfClass( Player.class, new AABB( pos ).inflate( 1.0F ) );
             boolean validNearbyTarget = false;
+            Predicate<Player> predicate = Config.MAIN.GENERAL.activateTrapsVsCreative.get()
+                    ? VS_CREATIVE_PREDICATE
+                    : DEFAULT_PREDICATE;
 
             for ( Player player : nearbyPlayers ) {
-                if ( !player.isCreative() && !player.isSpectator() ) {
+                if ( predicate.test( player ) ) {
                     validNearbyTarget = true;
                     break;
                 }
@@ -140,6 +138,24 @@ public class SeaMineBlock extends Block implements SimpleWaterloggedBlock {
     }
 
     @Override
+    public void onRemove( BlockState oldState, Level level, BlockPos pos, BlockState newState, boolean updateNeighbors ) {
+        super.onRemove( oldState, level, pos, newState, updateNeighbors );
+
+        if ( !newState.is( oldState.getBlock() ) ) {
+            explode(level, pos, level.random);
+        }
+    }
+
+    @Override
+    public InteractionResult use( BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult ) {
+        if ( level.isClientSide )
+            return InteractionResult.SUCCESS;
+
+        level.removeBlock( pos, false );
+        return InteractionResult.CONSUME;
+    }
+
+    @Override
     public FluidState getFluidState( BlockState state ) {
         return state.getValue( WATERLOGGED ) ? Fluids.WATER.getSource( false ) : super.getFluidState( state );
     }
@@ -147,5 +163,26 @@ public class SeaMineBlock extends Block implements SimpleWaterloggedBlock {
     @Override
     protected void createBlockStateDefinition( StateDefinition.Builder<Block, BlockState> builder ) {
         super.createBlockStateDefinition( builder.add( WATERLOGGED, ARMED ) );
+    }
+
+    private void explode( Level level, BlockPos pos, RandomSource random ) {
+        level.explode(
+                null,
+                pos.getX() + 0.5D,
+                pos.getY() + 0.5D,
+                pos.getZ() + 0.5D,
+                getExplosionPower(),
+                Level.ExplosionInteraction.BLOCK
+        );
+        // Pick a potion and apply it to all creatures caught in the blast
+        if ( getPotionList().isEmpty() ) return;
+
+        final MobEffectInstance potion = getPotionList().next( random );
+
+        if ( potion == null ) return;
+
+        List<LivingEntity> nearbyCreatures = level.getEntitiesOfClass( LivingEntity.class, new AABB( pos ).inflate( getExplosionPower() ) );
+        nearbyCreatures.forEach( (livingEntity) ->
+                livingEntity.addEffect( new MobEffectInstance( potion ) ) );
     }
 }
