195a6bcd851d5c83132d59f0c850080a7af9d967
FatherToast <father.toast@hotmail.com>
2025-11-01 13:26:12 -0500

    whoops reverting block thingy

--- src/main/java/fathertoast/deadlyworld/common/config/dimension/PitfallTrapConfig.java
+++ src/main/java/fathertoast/deadlyworld/common/config/dimension/PitfallTrapConfig.java
@@ -8,48 +8,45 @@ import fathertoast.deadlyworld.common.util.DimensionConfigHelper;
 import static fathertoast.deadlyworld.common.util.References.*;
 
 public class PitfallTrapConfig extends FeatureConfig {
-
+    
     public final PitfallTrapConfig.PitfallTrapTypeCategory SPIKES;
     public final PitfallTrapConfig.PitfallTrapTypeCategory LAVA;
     public final PitfallTrapConfig.PitfallTrapTypeCategory COBWEB;
-
-
+    
+    
     /** Builds the config spec that should be used for this config. */
     PitfallTrapConfig( ConfigManager manager, String dir, DimensionConfigGroup dimConfigs ) {
         super( manager, dir, dimConfigs, "pitfall trap" );
-
-        SPEC.newLine();
-
+        
         SPIKES = new PitfallTrapConfig.PitfallTrapTypeCategory( this, PitfallTrapType.SPIKES,
-                0.3, DEPTH_LAVA, DEPTH_1, 1, 4, 2, 4 );
+                0.3, DEPTH_LAVA, DEPTH_1, 2, 4, 2, 4 );
         LAVA = new PitfallTrapConfig.PitfallTrapTypeCategory( this, PitfallTrapType.LAVA,
-                0.3, DEPTH_LAVA, DEPTH_4, 1, 5, 2, 4 );
+                0.3, DEPTH_LAVA, DEPTH_4, 2, 5, 2, 4 );
         COBWEB = new PitfallTrapConfig.PitfallTrapTypeCategory( this, PitfallTrapType.COBWEB,
-                0.3, DEPTH_LAVA, DEPTH_1, 1, 4, 2, 4 );
+                0.3, DEPTH_LAVA, DEPTH_1, 2, 4, 2, 4 );
     }
-
+    
     public static class PitfallTrapTypeCategory extends FeatureTypeCategory {
-
+        
         public final IntField.RandomRange radius;
         public final IntField.RandomRange depth;
-
-
+        
         PitfallTrapTypeCategory( FeatureConfig parent, PitfallTrapType type,
-                               double placements, int minHeight, int maxHeight, int minRad, int maxRad, int minDpth, int maxDpth ) {
+                                 double placements, int minHeight, int maxHeight, int minRad, int maxRad, int minDpth, int maxDpth ) {
             this( parent, type.toString(), placements, minHeight, maxHeight, minRad, maxRad, minDpth, maxDpth );
         }
-
+        
         PitfallTrapTypeCategory( FeatureConfig parent, String name,
-                               double placements, int minHeight, int maxHeight, int minRad, int maxRad, int minDpth, int maxDpth ) {
+                                 double placements, int minHeight, int maxHeight, int minRad, int maxRad, int minDpth, int maxDpth ) {
             super( parent, name, placements, minHeight, maxHeight );
-
+            
             SPEC.newLine();
-
+            
             radius = new IntField.RandomRange( SPEC, "radius", minRad, maxRad, 1, 10,
                     "The minimum and maximum (inclusive) radius of the pit generated for this trap.",
                     "Note that a bigger radius will make it harder to pass generation criteria and make the trap less likely to generate.",
                     DimensionConfigHelper.MESSAGE_CONFIGURED_FEATURE_OVERRIDE );
-
+            
             depth = new IntField.RandomRange( SPEC, "depth", minDpth, maxDpth, 2, 6,
                     "The minimum and maximum (inclusive) depth of the pit generated for this trap",
                     "Note that a greater depth will make it harder to pass generation criteria and make the trap less likely to generate.",
--- src/main/java/fathertoast/deadlyworld/datagen/worldgen/DWAbstractCFProvider.java
+++ src/main/java/fathertoast/deadlyworld/datagen/worldgen/DWAbstractCFProvider.java
@@ -87,15 +87,15 @@ public abstract class DWAbstractCFProvider {
     public static final List<ResourceKey<ConfiguredFeature<?, ?>>> SEA_MINE_FEATURES = new ArrayList<>();
     /** List of all dungeon configurations. */
     public static final List<ResourceKey<ConfiguredFeature<?, ?>>> DUNGEON_FEATURES = new ArrayList<>();
-
+    
     /**
      * List of all configurations that don't care about dimension type and should generate anywhere,
      * AFTER {@link net.minecraft.world.level.levelgen.GenerationStep.Decoration#UNDERGROUND_DECORATION}.
      * Any restrictions are handled in the feature itself, usually config based.
      */
     public static final List<ResourceKey<ConfiguredFeature<?, ?>>> ANY_DIMENSION_POST_DECORATION = new ArrayList<>();
-
-
+    
+    
     private static final ResourceLocation EMPTY_RESOURCE_LOCATION = ResourceLocation.fromNamespaceAndPath( "", "" );
     
     // We aren't adding any "actual" ores, so we just use the base stone replace rules like dirt, etc. does
@@ -110,21 +110,21 @@ public abstract class DWAbstractCFProvider {
     
     /** Convenience method for making a simple block state provider. */
     protected static BlockStateProvider block( BlockState block ) { return BlockStateProvider.simple( block ); }
-
+    
     /** Convenience method for making a simple weighted random state provider with equal weights. */
     protected static BlockStateProvider blocks( BlockState... blocks ) {
         SimpleWeightedRandomList.Builder<BlockState> builder = new SimpleWeightedRandomList.Builder<>();
-
+        
         for( BlockState state : blocks ) {
             builder.add( state, 1 );
         }
         return new WeightedStateProvider( builder );
     }
-
+    
     /** Convenience method for making a simple weighted random state provider. */
     protected static BlockStateProvider weightedBlocks( Pair<BlockState, Integer>... stateWeightPairs ) {
         SimpleWeightedRandomList.Builder<BlockState> builder = new SimpleWeightedRandomList.Builder<>();
-
+        
         for( Pair<BlockState, Integer> pair : stateWeightPairs ) {
             builder.add( pair.getFirst(), pair.getSecond() );
         }
@@ -189,7 +189,7 @@ public abstract class DWAbstractCFProvider {
                         SpawnerSettings.of( type.getConfig( dimConfigs ) ),
                         BlockTags.FEATURES_CANNOT_REPLACE, vines ) ) );
     }
-
+    
     /** Registers a configured floor trap type feature to each supported dimension. */
     protected static void registerFloorTrap( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys.FloorTrap featureKeys,
                                              DimensionConfigGroup overworldConfigs, DimensionConfigGroup netherConfigs ) {
@@ -206,45 +206,43 @@ public abstract class DWAbstractCFProvider {
                         FloorTrapSettings.of( type.getConfig( dimConfigs ) ),
                         BlockTags.FEATURES_CANNOT_REPLACE ) ) );
     }
-
-
+    
+    
     /** Registers a configured spike trap type feature to each supported dimension. */
     protected static void registerSpikePatch( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys.SpikeTrap featureKeys,
                                               DimensionConfigGroup overworldConfigs, DimensionConfigGroup netherConfigs ) {
         registerSpikePatch( context, featureKeys.overworldKeys, featureKeys.trapType, overworldConfigs );
         registerSpikePatch( context, featureKeys.netherKeys, featureKeys.trapType, netherConfigs );
     }
-
+    
     /** Registers a configured spike trap type feature. */
     protected static void registerSpikePatch( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys featureKeys,
                                               SpikeTrapType type, DimensionConfigGroup dimConfigs ) {
         SpikeTrapConfig.SpikeTrapTypeCategory config = type.getConfig( dimConfigs );
-
+        
         register( context, featureKeys, new ConfiguredFeature<>( DWFeatures.SINGLE_BLOCK.get(),
                 new SpikeTrapPatchFeature.Configuration(
                         block( DWBlocks.spikeTrap( type ) ),
                         SpikePatchSettings.of( config ),
                         BlockTags.REPLACEABLE ) ) );
     }
-
+    
     /** Registers a configured pitfall trap type feature to each supported dimension. */
     protected static void registerPitfallTrap( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys.PitfallTrap featureKeys,
-                                               BlockStateProvider overworldCoverProvider, BlockStateProvider overworldFillProvider,
-                                               BlockStateProvider overworldFloorProvider,
-                                               BlockStateProvider netherCoverProvider, BlockStateProvider netherFillProvider,
-                                               BlockStateProvider netherFloorProvider,
-                                               DimensionConfigGroup overworldConfigs,
-                                               DimensionConfigGroup netherConfigs ) {
+                                               DimensionConfigGroup overworldConfigs, BlockStateProvider overworldCoverProvider,
+                                               BlockStateProvider overworldFillProvider, BlockStateProvider overworldFloorProvider,
+                                               DimensionConfigGroup netherConfigs, BlockStateProvider netherCoverProvider,
+                                               BlockStateProvider netherFillProvider, BlockStateProvider netherFloorProvider ) {
         registerPitfallTrap( context, featureKeys.overworldKeys, featureKeys.trapType,
-                overworldCoverProvider, overworldFillProvider, overworldFloorProvider, overworldConfigs );
+                overworldConfigs, overworldCoverProvider, overworldFillProvider, overworldFloorProvider );
         registerPitfallTrap( context, featureKeys.netherKeys, featureKeys.trapType,
-                netherCoverProvider, netherFillProvider, netherFloorProvider, netherConfigs );
+                netherConfigs, netherCoverProvider, netherFillProvider, netherFloorProvider );
     }
-
+    
     /** Registers a configured pitfall trap type feature. */
     protected static void registerPitfallTrap( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys featureKeys,
-                                               PitfallTrapType type, BlockStateProvider coverProvider, BlockStateProvider fillProvider,
-                                               BlockStateProvider floorProvider, DimensionConfigGroup dimConfigs ) {
+                                               PitfallTrapType type, DimensionConfigGroup dimConfigs, BlockStateProvider coverProvider,
+                                               BlockStateProvider fillProvider, BlockStateProvider floorProvider ) {
         register( context, featureKeys, new ConfiguredFeature<>( DWFeatures.PITFALL_TRAP.get(),
                 new PitfallTrapFeature.Configuration(
                         coverProvider, fillProvider, floorProvider,
@@ -257,13 +255,13 @@ public abstract class DWAbstractCFProvider {
     protected static void registerTower( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys.TowerDispenser featureKeys,
                                          DimensionConfigGroup overworldConfigs, BlockStateProvider overworldBase,
                                          DimensionConfigGroup netherConfigs, BlockStateProvider netherBase ) {
-        registerTower( context, featureKeys.overworldKeys, featureKeys.towerType, overworldBase, overworldConfigs );
-        registerTower( context, featureKeys.netherKeys, featureKeys.towerType, netherBase, netherConfigs );
+        registerTower( context, featureKeys.overworldKeys, featureKeys.towerType, overworldConfigs, overworldBase );
+        registerTower( context, featureKeys.netherKeys, featureKeys.towerType, netherConfigs, netherBase );
     }
     
     /** Registers a configured tower dispenser type feature. */
     protected static void registerTower( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys featureKeys,
-                                         TowerType type, BlockStateProvider base, DimensionConfigGroup dimConfigs ) {
+                                         TowerType type, DimensionConfigGroup dimConfigs, BlockStateProvider base ) {
         register( context, featureKeys, new ConfiguredFeature<>( DWFeatures.TOWER.get(),
                 new TowerFeature.Configuration( block( DWBlocks.towerDispenser( type ) ), base,
                         TowerDispenserSettings.of( type.getConfig( dimConfigs ) ),
@@ -274,20 +272,20 @@ public abstract class DWAbstractCFProvider {
     /** Registers a configured registerSeaMine type feature to each supported dimension. */
     protected static void registerSeaMine( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys.SeaMine featureKeys,
                                            DimensionConfigGroup overworldConfigs ) {
-        registerSeaMine( context, featureKeys.overworldKeys, featureKeys.seaMineType, block( Blocks.CHAIN.defaultBlockState()
-                        .setValue( ChainBlock.AXIS, Direction.Axis.Y ).setValue( ChainBlock.WATERLOGGED, true ) ),
-                overworldConfigs );
+        registerSeaMine( context, featureKeys.overworldKeys, featureKeys.seaMineType, overworldConfigs,
+                block( Blocks.CHAIN.defaultBlockState().setValue( ChainBlock.AXIS, Direction.Axis.Y )
+                        .setValue( ChainBlock.WATERLOGGED, true ) ) );
     }
     
     /** Registers a configured sea mine type feature. */
     protected static void registerSeaMine( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys featureKeys,
-                                           SeaMineType type, BlockStateProvider trailProvider, DimensionConfigGroup dimConfigs ) {
+                                           SeaMineType type, DimensionConfigGroup dimConfigs, BlockStateProvider trailProvider ) {
         register( context, featureKeys, new ConfiguredFeature<>( DWFeatures.SEA_MINE.get(),
                 new SeaMineFeature.Configuration( block( DWBlocks.seaMine( type ).get().defaultBlockState()
                         .setValue( SeaMineBlock.WATERLOGGED, true ) ), trailProvider,
                         SeaMineSettings.of( type.getConfig( dimConfigs ) ) ) ) );
     }
-
+    
     /** Registers a configured feature. */
     protected static void register( BootstapContext<ConfiguredFeature<?, ?>> context, FeatureKeys featureKeys, ConfiguredFeature<?, ?> configuredFeature ) {
         context.register( featureKeys.configuredKey, configuredFeature );
@@ -305,7 +303,7 @@ public abstract class DWAbstractCFProvider {
         ANY_DIMENSION_ORE_FEATURES.add( key );
         return key;
     }
-
+    
     /** Creates a configured key for an "any dimension" post-decor feature. */
     protected static ResourceKey<ConfiguredFeature<?, ?>> anyDimPostDecor( String name ) {
         ResourceKey<ConfiguredFeature<?, ?>> key = key( name + "_any_dimension_post_decoration" );
