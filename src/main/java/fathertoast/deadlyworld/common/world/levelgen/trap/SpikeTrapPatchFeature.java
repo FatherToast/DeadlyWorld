e1a53ab09fb49142eff90fc87b3569ece6355378
FatherToast <father.toast@hotmail.com>
2025-11-02 11:39:03 -0600

    Spike trap tweaks

--- src/main/java/fathertoast/deadlyworld/common/world/levelgen/trap/SpikeTrapPatchFeature.java
+++ src/main/java/fathertoast/deadlyworld/common/world/levelgen/trap/SpikeTrapPatchFeature.java
@@ -2,11 +2,11 @@ package fathertoast.deadlyworld.common.world.levelgen.trap;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import fathertoast.deadlyworld.common.world.levelgen.ChestSettings;
+import fathertoast.deadlyworld.common.block.spike_trap.BaseSpikeTrapBlock;
+import fathertoast.deadlyworld.common.config.dimension.FeatureConfig;
 import fathertoast.deadlyworld.common.world.levelgen.SpikePatchSettings;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
@@ -24,39 +24,35 @@ public class SpikeTrapPatchFeature extends DeadlyFeature<SpikeTrapPatchFeature.C
             SpikePatchSettings patchSettings,
             TagKey<Block> canReplace
     ) implements FeatureConfiguration {
-        public static final Codec<SpikeTrapPatchFeature.Configuration> CODEC = RecordCodecBuilder.create( (instance ) -> instance.group(
-                BlockStateProvider.CODEC.fieldOf( "spikes_provider" ).forGetter( SpikeTrapPatchFeature.Configuration::spikesProvider ),
-                SpikePatchSettings.CODEC.fieldOf( "patch_settings" ).forGetter( SpikeTrapPatchFeature.Configuration::patchSettings ),
-                TagKey.hashedCodec( Registries.BLOCK ).fieldOf( "can_replace" ).forGetter( SpikeTrapPatchFeature.Configuration::canReplace )
-        ).apply( instance, SpikeTrapPatchFeature.Configuration::new ) );
+        public static final Codec<Configuration> CODEC = RecordCodecBuilder.create( ( instance ) -> instance.group(
+                BlockStateProvider.CODEC.fieldOf( "spikes_provider" ).forGetter( Configuration::spikesProvider ),
+                SpikePatchSettings.CODEC.fieldOf( "patch_settings" ).forGetter( Configuration::patchSettings ),
+                TagKey.hashedCodec( Registries.BLOCK ).fieldOf( "can_replace" ).forGetter( Configuration::canReplace )
+        ).apply( instance, Configuration::new ) );
     }
-
-    public SpikeTrapPatchFeature() {
-        this( SpikeTrapPatchFeature.Configuration.CODEC );
-    }
-
-    public SpikeTrapPatchFeature(Codec<SpikeTrapPatchFeature.Configuration> codec ) {
-        super( codec );
-    }
-
+    
+    public SpikeTrapPatchFeature() { this( Configuration.CODEC ); }
+    
+    public SpikeTrapPatchFeature( Codec<Configuration> codec ) { super( codec ); }
+    
     @Override
-    public boolean place( FeaturePlaceContext<SpikeTrapPatchFeature.Configuration> context ) {
+    public boolean place( FeaturePlaceContext<Configuration> context ) {
         final boolean notSubfeature = context.topFeature().isEmpty();
-        final SpikeTrapPatchFeature.Configuration config = context.config();
+        final Configuration config = context.config();
         final RandomSource random = context.random();
         final BlockPos origin = context.origin();
         final WorldGenLevel level = context.level();
-        final Predicate<BlockState> predicate = (state) -> state.is( config.canReplace );
-
-
-        BlockPos.MutableBlockPos cursor = new BlockPos.MutableBlockPos();
-
-        int placedBlocks = 0;
+        final Predicate<BlockState> predicate = ( state ) -> state.is( config.canReplace );
+        
+        final BlockPos.MutableBlockPos cursor = new BlockPos.MutableBlockPos();
+        
         int xzSpread = config.patchSettings.xzSpread().sample( random );
         int ySpread = config.patchSettings.ySpread().sample( random );
         int patchSize = config.patchSettings.patchSize().sample( random );
-
+        
         // Place spike patch
+        BlockState spikes = null;
+        int placedBlocks = 0;
         for( int tries = 0; tries < patchSize; ++tries ) {
             cursor.setWithOffset(
                     origin,
@@ -64,28 +60,28 @@ public class SpikeTrapPatchFeature extends DeadlyFeature<SpikeTrapPatchFeature.C
                     random.nextInt( ySpread ) - random.nextInt( ySpread ),
                     random.nextInt( xzSpread ) - random.nextInt( xzSpread )
             );
-
-            if ( notSubfeature ) {
-                // Make sure the chest block can be placed
-                if ( !predicate.test( level.getBlockState( cursor ) ) ) continue;
+            
+            if( notSubfeature ) {
                 // Don't replace blocks with block entities
-                if ( level.getExistingBlockEntity( cursor ) != null ) continue;
+                if( level.getExistingBlockEntity( cursor ) != null ) continue;
             }
-
-            // Place spike trap
-            BlockState spikes = config.spikesProvider.getState( random, cursor );
-
-            // Make sure the location is valid for the picked spikes
-            if ( spikes.canSurvive( level, cursor ) ) {
-                setBlock(level, cursor, config.spikesProvider, random);
+            
+            // Try to place spike trap
+            spikes = config.spikesProvider.getState( random, cursor );
+            if( predicate.test( level.getBlockState( cursor ) ) && spikes.canSurvive( level, cursor ) ) {
+                setBlock( level, cursor, spikes );
                 ++placedBlocks;
             }
         }
-
-        // Generate debug marker
-        if ( placedBlocks > 0 && config.patchSettings.debugMarker().get() )
-            debugMarker( level, origin );
-
-        return placedBlocks > 0;
+        
+        if( placedBlocks > 0 ) {
+            // Generate debug marker
+            if( spikes.getBlock() instanceof BaseSpikeTrapBlock spikeTrapBlock ) {
+                final FeatureConfig.FeatureTypeCategory featureConfig = spikeTrapBlock.getSpikeTrapType().getConfig( level.getLevel() );
+                debugMarkerIfEnabled( level, cursor, featureConfig );
+            }
+            return true;
+        }
+        return false;
     }
 }
